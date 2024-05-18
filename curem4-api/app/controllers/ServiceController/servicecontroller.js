3d154da14e917eb2d3063bf5f5e339de67b043df
vyas2610 <deepakvyas261092@gmail.com>
2024-05-18 17:55:52 +0530

    Services API. CURD OPR

--- curem4-api/app/controllers/ServiceController/servicecontroller.js
+++ curem4-api/app/controllers/ServiceController/servicecontroller.js
@@ -1,35 +1,95 @@
-function index(req, res) {
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+async function index(req, res) {
     //get all the data
-    res.send({
-        msg: "Data Fetch..!"
-    })
+
+    try {
+        let response = await prisma.services.findMany();
+        res.send({
+            msg: response
+        })
+    } catch (error) {
+        res.status(500).send({
+            error
+        })
+    }
 
 }
 
-function create(req, res) {
+async function create(req, res) {
+    try {
+        await prisma.services.create({
+            data: req.body
+        })
+        res.send({
+            msg: "Data Added..!"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error
+        })
+    }
     //add new data
-    res.send({
-        msg: "Data Added..!"
-    })
 }
-function update(req, res) {
-    // update data
-    res.send({
-        mag: "Data Updated..!"
-    })
-}
-function details(req, res) {
-    // detail of a data accorind to id
-    res.send({
-        msg: "Details Fetch..!"
-    })
-}
-function destroy(req, res) {
-    // delete a data according to id
+async function update(req, res) {
 
-    res.send({
-        msg: "Delete...!"
-    })
+    // update data
+    try {
+        await prisma.services.update({
+            where: {
+                ser_id: parseInt(req.params.id),
+            },
+            data: req.body
+
+        })
+        res.send({
+            msg: "Date updated...!"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error
+        })
+    }
+}
+async function details(req, res) {
+    // detail of a data accorind to id
+    try {
+        let response = await prisma.services.findFirst({
+            where: {
+                ser_id: parseInt(req.params.id)
+            }
+        })
+        res.send({
+            response
+        })
+    } catch (error) {
+        res.status(500).send({
+            error
+        })
+    }
+}
+async function destroy(req, res) {
+    // delete a data according to id
+    try {
+        await prisma.services.delete({
+            where: {
+                ser_id: parseInt(req.params.id)
+            }
+        })
+        res.send({
+            msg: "Delete...!"
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            error
+        })
+    }
 }
 
 module.exports = { index, create, details, destroy, update }
